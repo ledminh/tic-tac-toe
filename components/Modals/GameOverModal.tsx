94f4e278211e2be63c337c3826c995481c253830
Minh Le <duyminh.le310@gmail.com>
2022-10-07 17:56:48 -0400

    GameOverModal

--- components/Modals/GameOverModal.tsx
+++ components/Modals/GameOverModal.tsx
@@ -4,6 +4,8 @@ import Modal from "./Modal";
 
 import styles from './GameOverModal.module.scss';
 
+import XIcon from '../../assets/icon-x.svg';
+import OIcon from '../../assets/icon-o.svg';
 
 const GameOverModal:FunctionComponent<{showModal: boolean, setShowModal: (show:boolean) => void}> = ({showModal, setShowModal}) => {
 
@@ -12,7 +14,20 @@ const GameOverModal:FunctionComponent<{showModal: boolean, setShowModal: (show:b
         <Modal onClose={() => setShowModal(false)}
                 show={showModal}
         >
-            <span>GameOver</span>
+            <div className={styles.line1}>PLAYER 1 WINS!</div>
+            <div className={styles.line2}>
+                <div className={styles.icon}>
+                    <XIcon
+                        viewBox="0 0 64 64"
+                    />
+                </div>
+                <span>TAKES THE ROUND</span>
+            </div>
+            {/* <div className={styles.roundTies}><span>ROUND TIES</span></div> */}
+            <div className={styles.buttons}>
+                <button className={styles.quit}>QUIT</button>
+                <button className={styles.nextRound}>NEXT ROUND</button>
+            </div>            
         </Modal>
     )
 }
