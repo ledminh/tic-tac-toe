6d59a02434b7a09ab561a61dda2df32997b7167f
Minh Le <duyminh.le310@gmail.com>
2022-10-07 18:41:07 -0400

    Create DataContext

--- Contexts.tsx
+++ Contexts.tsx
@@ -1,15 +1,17 @@
 import { FunctionComponent } from "react";
 
 import useUI, {UIContext} from './useUI';
-
+import useData, {DataContext} from './useData';
 
 const Contexts:FunctionComponent<{children:JSX.Element[]}> = ({children}) => {
     const uiControls = useUI();
-
+    const data = useData();
     return (
-        <UIContext.Provider value={uiControls}>
-            {children}    
-        </UIContext.Provider>
+        <DataContext.Provider value={data}>
+            <UIContext.Provider value={uiControls}>
+                {children}    
+            </UIContext.Provider>
+        </DataContext.Provider>
     )
 }
 
---
+++ useData/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+
+type DataType = {
+    
+}
+
+const defaultData:DataType = {
+    
+}
+
+export const DataContext = React.createContext<DataType>(defaultData);
+
+
+const useData = () => {
+
+
+
+
+    return {
+    }
+}
+
+
+export default useData;
